1e4c76432939b5a41ee0948f45b733a23f17e760
Limjiyoung <22314671@naver.com>
2023-04-12 00:27:09 +0900

    움직임 진행중

--- Source/MonG/Private/MonGPlayer.cpp
+++ Source/MonG/Private/MonGPlayer.cpp
@@ -2,12 +2,21 @@
 
 
 #include "MonGPlayer.h"
+#include <../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h>
+#include "EnhancedInputSubsystems.h"
+#include <Engine/LocalPlayer.h>
+#include <Camera/CameraComponent.h>
 
 // Sets default values
 AMonGPlayer::AMonGPlayer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+
+
+	//Ä«¸Þ¶ó
+	camera = CreateDefaultSubobject<UCameraComponent>(TEXT("camera"));
 
 }
 
@@ -15,7 +24,18 @@ AMonGPlayer::AMonGPlayer()
 void AMonGPlayer::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	playerController = Cast<APlayerController>(GetWorld()->GetFirstPlayerController());
+
+	if (playerController)
+	{
+		auto localPlayer = playerController->GetLocalPlayer();
+		auto subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(localPlayer);
+		if (subSystem)
+		{
+			subSystem->AddMappingContext(IMC_MonGInput, 0);
+		}
+	}
 }
 
 // Called every frame
@@ -30,7 +50,20 @@ void AMonGPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* inputSystem = Cast <UEnhancedInputComponent>(PlayerInputComponent);
+	if (inputSystem)
+	{
+		inputSystem->BindAction(IA_MonGMove, ETriggerEvent::Triggered, this, &AMonGPlayer::Move);
+	}
 
+}
+
+void AMonGPlayer::Move(const FInputActionValue& Values)
+{
+	UE_LOG(LogTemp, Warning, TEXT("mmmoveee"));
+	FVector2D axis = Values.Get<FVector2D>();
+	AddMovementInput(FVector(1, 0, 0), axis.X);
+	AddMovementInput(FVector(0, 1, 0), axis.Y);
 
 }
 
--- Source/MonG/Public/MonGPlayer.h
+++ Source/MonG/Public/MonGPlayer.h
@@ -27,10 +27,18 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	//동작
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputMappingContext* IMC_Input;
-
+	class UInputMappingContext* IMC_MonGInput;
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* IA_Move;
+	class UInputAction* IA_MonGMove;
+	//카메라
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UCameraComponent* camera;
 
+	UPROPERTY()
+	class APlayerController* playerController;
+
+	UFUNCTION()
+	void Move(const FInputActionValue& Values);
 };
